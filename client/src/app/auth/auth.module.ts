b69b7706d078deb21169b1af0aafd0ab0d76b42d
NaN-niki <niki_stst@abv.bg>
2024-04-10 17:58:05 +0300

    Auth routing module created

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { ButtonComponent } from '../shared/button/button.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent,
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule,
+    SharedModule,
+    RouterModule
   ]
 })
 export class AuthModule { }

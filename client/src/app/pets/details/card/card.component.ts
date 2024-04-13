9477201a1e3b48b0e5a4f807b934c6f487e1db5e
NaN-niki <niki_stst@abv.bg>
2024-04-14 01:40:21 +0300

    Edit component form added

--- client/src/app/pets/details/card/card.component.ts
+++ client/src/app/pets/details/card/card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 import { IPet } from 'src/app/interfces';
+import { PetService } from '../../pet.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -10,4 +13,21 @@ export class CardComponent {
   @Input() pet! : IPet
   editBtn : string = 'Edit'
   deleteBtn : string = 'Delete'
+
+  constructor(private authService : AuthService, private petService : PetService, private router: Router){}
+
+  get owner(){
+    return this.authService.user?._id == this.pet.owner
+  }
+
+  deleteHandler() {
+    if(window.confirm('Are you sure you want to delete this?')){   /// TOAST
+      this.petService.deletePet(this.pet._id).subscribe({
+        next: () => {
+          this.router.navigate(['/pets'])
+        }
+      })
+    }
+    
+  }
 }
--- client/src/app/pets/edit/edit.component.ts
+++ client/src/app/pets/edit/edit.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { PetService } from '../pet.service';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IPet } from 'src/app/interfces';
 
 @Component({
   selector: 'app-edit',
@@ -11,11 +12,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class EditComponent {
   @Input() submitBtn = 'Submit'
   id = this.activatedRoute.snapshot.params['id']
+  pet! : IPet
 
   constructor(private petService: PetService, private router: Router, private activatedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
-
+    this.petService.getOnePet(this.id).subscribe(pet => this.pet = pet)
   }
 
   editHandler(form: NgForm) {
@@ -23,14 +25,14 @@ export class EditComponent {
 
     const { animalType, breed, name, skin_color, imageUrl, description, contactInfo, address } = form.value
 
-    // this.petService.editPet(animalType, breed, name, skin_color, imageUrl, description, contactInfo, address, this.id).subscribe({
-    //   next: () => {
-    //     this.router.navigate([`/${this.id}/details`])
-    //   },
-    //   error: () => {
-    //     console.log()
-    //   }
-    // })
+    this.petService.editPet(animalType, breed, name, skin_color, imageUrl, description, contactInfo, address, this.id).subscribe({
+      next: () => {
+        this.router.navigate([`/${this.id}/details`])
+      },
+      error: () => {
+        console.log()
+      }
+    })
   }
 
 }

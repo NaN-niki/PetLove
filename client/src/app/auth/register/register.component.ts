cca59915e91dabca7cce409c5cebc66fbdbc0fd3
NaN-niki <niki_stst@abv.bg>
2024-04-13 13:04:41 +0300

    Register component validation and handler added

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { matchPasswordGroupValidator } from 'src/app/shared/validators/matchPassword';
+import { usernameValidatorFn } from 'src/app/shared/validators/usernameValidatorFn';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,23 @@ import { Component, Input } from '@angular/core';
 })
 export class RegisterComponent {
   @Input() submitBtn: string = 'Submit'
+
+  registerForm = this.fb.group({
+    username: ['', [Validators.required, usernameValidatorFn()]],
+    firstName: ['', [Validators.required, Validators.minLength(2)]],
+    lastName: ['', [Validators.required, Validators.minLength(2)]],
+    email : ['', [Validators.required, Validators.email]],
+    pass: this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rePass: ['']
+    }, {
+      validators : [matchPasswordGroupValidator('password', 'rePass')]
+    })
+  })
+
+  constructor(private fb : FormBuilder){}
+
+  registerHandler(){
+
+  }
 }

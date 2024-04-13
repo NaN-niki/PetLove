a860835d6f6778bd9a609150ca0573fe22b2af3c
NaN-niki <niki_stst@abv.bg>
2024-04-13 15:05:59 +0300

    Add pet and edir pet functionalities added t petService

--- client/src/app/pets/pet.service.ts
+++ client/src/app/pets/pet.service.ts
@@ -14,4 +14,12 @@ export class PetService {
 
   getAllPets = () => this.http.get<IPet[]>(this.apiUrl)
   getOnePet = (petId: string) => this.http.get<IPet>(this.apiUrl + '/' + petId)
+  addPet = (animalType : string, breed: string, name: string, skin_color:string, imageUrl: string, description:string, contactInfo: number, address: number) => 
+  this.http.post(`${this.apiUrl}/create`, {
+    animalType, breed, name, skin_color, imageUrl, description, contactInfo, address
+  })
+  editPet = (animalType : string, breed: string, name: string, skin_color:string, imageUrl: string, description:string, contactInfo: number, address: number, id: string) => 
+  this.http.put(`${this.apiUrl}/${id}`, {
+    animalType, breed, name, skin_color, imageUrl, description, contactInfo, address
+  })
 }

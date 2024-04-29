57f56fe37b10f14796ccd7b1b230a0ac50cf8710
NaN-niki <niki_stst@abv.bg>
2024-04-29 19:16:38 +0300

    cors fixes

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+    res.setHeader('Access-Control-Allow-Origin', 'https://pet-love-ft8x.vercel.app')
     res.setHeader('Access-Control-Allow-Credentials', 'true')
     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization')
